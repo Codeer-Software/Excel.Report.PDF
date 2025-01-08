b7e88fc1b3207a14b231557e383da38828697f20
采乃 和田 <wada-ayano@codeer.co.jp>
2025-01-08 16:42:39 +0900

    Added tests

--- Source/Test/Test/RangeTest.cs
+++ Source/Test/Test/RangeTest.cs
@@ -6,25 +6,48 @@ namespace Test
     public class RangeTest
     {
         [Test]
-        public void Test()
+        public void AddRowTest()
         {
-            //TODO excel
-
-            using (var stream = new FileStream(Path.Combine(TestEnvironment.PdfSrcPath, "xxx.xlsx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var stream = new FileStream(Path.Combine(TestEnvironment.PdfSrcPath, "RangeTestAddRow.xlsx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             using (var book = new XLWorkbook(stream))
             {
-                //TODO Add rows
+                // Add rows
+                var worksheet = book.Worksheet(1);
+                worksheet.Row(1).InsertRowsAbove(1);
+                worksheet.Cell("A1").Value = "NewCellData";
 
-                book.SaveAs(Path.Combine(TestEnvironment.TestResultsPath, "xxx2.xlsx"));
+                book.SaveAs(Path.Combine(TestEnvironment.TestResultsPath, "ResultRangeTestAddRow.xlsx"));
 
             }
 
-            using (var stream = new FileStream(Path.Combine(TestEnvironment.TestResultsPath, "xxx.xlsx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var stream = new FileStream(Path.Combine(TestEnvironment.TestResultsPath, "ResultRangeTestAddRow.xlsx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             using (var book = new OpenClosedXML(stream))
             {
                 book.GetSheetMaxRowCol(1, out var rowCount, out var colCount);
 
-                //TODO check
+                // Check whether the number of rows and columns can be obtained correctly even if a row is added
+                rowCount.Is(5);
+                colCount.Is(1);
+            }
+        }
+
+        [Test]
+        public void StyleCellTest()
+        {
+            using (var stream = new FileStream(Path.Combine(TestEnvironment.PdfSrcPath, "RangeTestStyleCell.xlsx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var book = new XLWorkbook(stream))
+            {
+                book.SaveAs(Path.Combine(TestEnvironment.TestResultsPath, "ResultRangeTestStyleCell.xlsx"));
+            }
+
+            using (var stream = new FileStream(Path.Combine(TestEnvironment.TestResultsPath, "ResultRangeTestStyleCell.xlsx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var book = new OpenClosedXML(stream))
+            {
+                book.GetSheetMaxRowCol(1, out var rowCount, out var colCount);
+
+                // Check whether the number of rows and columns can be obtained correctly even if there is a cell with only style
+                rowCount.Is(10);
+                colCount.Is(1);
             }
         }
     }
